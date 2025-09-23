48329f63e4278296dfb10407dccda21cb88bea34
Dinithi Sonali Gamage <75924676+Dini-s@users.noreply.github.com>
2025-09-23 15:53:20 +0530

    Update cloudinary.js

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -1,14 +1,7 @@
 <<<<<<< HEAD
-import {v2 as cloudinary} from 'cloudinary'
-
-const connectCloudinary = async () =>{
-=======
 import { v2 as cloudinary } from 'cloudinary'
-import dotenv from "dotenv";
 
 const connectCloudinary = async () => {
-  dotenv.config();
->>>>>>> 0755a86 (modify report route)
 
   cloudinary.config({
     cloud_name: process.env.CLOUDINARY_NAME,
