6694f0794189382491f304ce53d0a8edcf4c0e86
Dinithi Sonali Gamage <75924676+Dini-s@users.noreply.github.com>
2025-09-23 15:49:50 +0530

    update mongoDb

--- config/mongodb.js
+++ config/mongodb.js
@@ -1,21 +1,9 @@
 import mongoose from "mongoose";
 
-<<<<<<< HEAD
-const connectDB = async () =>{
-    mongoose.connection.on('connected', ()=> console.log("Database Connected"))
-=======
 const connectDB = async () => {
-  try {
-    mongoose.connection.on('connected', () => console.log("Database Connected"))
-    await mongoose.connect(`${process.env.MONGODB_URL}/AquaShield`)
-  } catch (error) {
-    console.error(error);
-    process.exit(1);
-  }
-};
->>>>>>> 0755a86 (modify report route)
+  mongoose.connection.on('connected', () => console.log("Database Connected"))
 
-    await mongoose.connect(`${process.env.MONGODB_URI}/AquaShield`)
+  await mongoose.connect(`${process.env.MONGODB_URI}/AquaShield`)
 }
 
 export default connectDB
