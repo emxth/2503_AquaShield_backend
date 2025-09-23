7950d858fc0388a835c88b508b948f3b64a64db0
Dinithi Sonali Gamage <75924676+Dini-s@users.noreply.github.com>
2025-09-23 16:14:17 +0530

    update connectDb

--- config/mongodb.js
+++ config/mongodb.js
@@ -9,10 +9,6 @@ const connectDB = async () => {
     process.exit(1);
   }
 };
-const connectDB = async () => {
-  mongoose.connection.on('connected', () => console.log("Database Connected"))
 
-  await mongoose.connect(`${process.env.MONGODB_URI}/AquaShield`)
-}
 
 export default connectDB
