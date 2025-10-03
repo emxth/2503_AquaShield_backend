cfbeef3ac02ef740944adc863eb281c94feaf6e8
Emith Randiv <randivemith.2003@gmail.com>
2025-10-03 13:24:58 +0530

    Modify routes for dashboard content

--- routes/reportRoutes.js
+++ routes/reportRoutes.js
@@ -1,15 +1,22 @@
 import { Router } from "express";
 import { uploadMulter } from "../middleware/uploadMulter.js";
-<<<<<<< Updated upstream
-import { createNewReport, deleteReport, getAllReports, getIncidentTypes, getSubmittedReports, reportFilterBySatatus, updateReports } from "../controller/reportController.js";
 import _default from "validator";
-=======
-import { createNewReport, deleteReport, getAllReports, getIncidentTypes, getSpecificReports, getSubmittedReports, reportFilterBySatatus, updateReports } from "../controller/reportController.js";
->>>>>>> Stashed changes
+import { 
+  createNewReport, 
+  deleteReport, 
+  getAllReports, 
+  getIncidentTypes, 
+  getSubmittedReports, 
+  reportFilterBySatatus, 
+  updateReports,
+  getRecentReports,
+  getTrendData,
+  getSpeciesData,
+  getMonthlyStats
+} from "../controller/reportController.js";
 
 const reportRouter = Router();
 
-<<<<<<< Updated upstream
 reportRouter.post("/create", uploadMulter.array("files"), createNewReport);
 reportRouter.get("/getReports", getSubmittedReports);
 reportRouter.get("/getAllReports", getAllReports);
@@ -17,16 +24,9 @@ reportRouter.get("/filteredReport", reportFilterBySatatus);
 reportRouter.get("/incidentType", getIncidentTypes);
 reportRouter.put("/updateReport", updateReports);
 reportRouter.delete("/deleteReport", deleteReport);
-
-=======
-router.post("/createReports", uploadMulter.single("file"), createNewReport);
-router.get("/getReports", getSubmittedReports);
-router.get("/getAllReports", getAllReports);
-router.get("/filteredReport", reportFilterBySatatus);
-router.get("/incidentType", getIncidentTypes);
-router.put("/updateReport", updateReports);
-router.delete("/deleteReport", deleteReport);
-router.get("/getSpecificReport/:id", getSpecificReports);
->>>>>>> Stashed changes
+reportRouter.get("/recent", getRecentReports);
+reportRouter.get("/trends", getTrendData);
+reportRouter.get("/species", getSpeciesData);
+reportRouter.get("/monthly-stats", getMonthlyStats);
 
 export default reportRouter;
