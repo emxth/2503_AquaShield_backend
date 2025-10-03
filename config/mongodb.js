72fdedf4464b8d92f72ef4e582551b00e9e911bd
Emith Randiv <randivemith.2003@gmail.com>
2025-10-03 13:26:32 +0530

    Update mogoDB env calling

--- config/mongodb.js
+++ config/mongodb.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   try {
     mongoose.connection.on('connected', () => console.log("Database Connected"))
-    await mongoose.connect(`${process.env.MONGODB_URL}/AquaShield`)
+    await mongoose.connect(`${process.env.MONGODB_URI}/AquaShield`)
   } catch (error) {
     console.error(error);
     process.exit(1);
