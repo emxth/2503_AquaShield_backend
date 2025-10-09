ffa9fe74538fc155b82103136179df35a6d0d472
Poojani K.H.S <sahanyapoojani.k.h@gmail.com>
2025-10-09 13:25:07 +0530

    commit

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.get('/',(req,res)=>{
   res.send('API Working')
 })
 
-// Start server
-app.listen(port, '0.0.0.0', () => {
-  console.log(`Server running on http://0.0.0.0:${port}`);
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
+
