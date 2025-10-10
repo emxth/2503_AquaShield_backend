41deacc524e769a46f06f18c46bd4856fa493fb5
Poojani K.H.S <sahanyapoojani.k.h@gmail.com>
2025-10-11 00:02:08 +0530

    Reapply "all commits"
    This reverts commit 535e4a4643a328bb9cf672ce64eee7482bd8690e.

---
+++ routes/passwordRoute.js
@@ -0,0 +1,10 @@
+import express from "express";
+import { sendOTP, verifyOTP, resetPassword } from "../controllers/passwordController.js";
+
+const passwordRouter = express.Router();
+
+passwordRouter.post("/forgot-password", sendOTP);
+passwordRouter.post("/verify-otp", verifyOTP);
+passwordRouter.post("/reset-password", resetPassword);
+
+export default passwordRouter
